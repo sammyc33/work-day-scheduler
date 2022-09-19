e327287e38985a180505133d06e4a7cd5d1ee86e
Sammy <sammy.campbell33@gmail.com>
2022-09-18 18:41:05 -0700

    add js functions for time block color and event storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,53 @@
 //Display today's date in the header
 var currentDate = moment().format('dddd, MMM Do, YYYY, LTS');
-$("#currentDay").html(currentDate);
+$("#currentDay").text(currentDate);
 
-//Declare remaining variables
-var saveBtn
+//Declare remaining variable(s)
+var saveBtn = $(".saveBtn");
+var timeBlock = $(".time-block");
+var now = moment().hours();
+var hour = $(".hour");
+var sched = $(".sched");
 
-// Time bolocks for standard business hours are displayed and tiems are color-coded
+// Time blocks for standard business hours are displayed and times are color-coded
+function changeColors() {
+   timeBlock.each(function () {
+        var hour = parseInt($(this).attr("id"));
+        if (hour < now) {
+            $(this).addClass("past");
+            $(this).removeClass("present");
+            $(this).removeClass("future");
 
+        } else if (hour === now) {
+            $(this).addClass("present");
+            $(this).removeClass("past");
+            $(this).removeClass("future")
 
-// Click into the time block to add text for the event, use the save button to save the event
+        } else {
+            $(this).addClass("future");
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+        }
+    })
+};
 
+// When you add event text and click save, the event changes are saved in local storage
+saveBtn.on("click", function () {
+    var time = $(this).siblings(hour).text();
+    var text = $(this).siblings(sched).val();
+    localStorage.setItem(time, text);
+});
 
-// When the page is refreshed, the changes persist
-
+function showSchedule() {
+    hour.each(function () {
+        var theTime = $(this).text();
+        var theSchedule = localStorage.getItem(theTime);
+        if (theSchedule !== null) {
+            $(this).siblings(sched).val(theTime);
+        }
+    });
+}
 
 // Remember to call the functions!
+changeColors();
+showSchedule();
